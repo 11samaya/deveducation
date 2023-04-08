88a573366908972277df06020dd0a1565f6907a3
11samaya <samayavtx@gmail.com>
2023-04-08 20:12:48 +0400

    homework2

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine();
+        }
+    }
+    namespace arrays
+    {
+        public class Program
+        {
+            public static void Main()
+            {
+                string str = "Lorem sit ametist uwewuatokua";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    int count = 0;
+                    int temp = i - 1;
+                    bool flag = false;
+                    while (temp > 0)
+                    {
+                        if (str[temp] == str[i])
+                        {
+                            flag = true;
+                        }
+                        temp--;
+                    }
+                    if (flag == false)
+                    {
+                        for (int j = 0; j < str.Length; j++)
+                        {
+                            if (str[i] == str[j])
+                            {
+                                count++;
+                            }
+                        }
+
+                        Console.WriteLine(str[i] + "-" + count);
+                    }
+                }
+            }
+        }
+    }
+}
