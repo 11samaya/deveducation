29b4c6a9b15c4184dbe7c6ecc505370f023cb624
11samaya <samayavtx@gmail.com>
2023-04-13 18:43:15 +0400

    old hw

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string str = "anysentence";
+            {
+            for (int i = 0; i < str.Length; i++)
+            {
+                bool hadPrevious = false;
+                char symbol = str[i];
+                for (int j = i; j >= 0; j--)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+                    char prevSymbol = str[j];
+
+                    if (prevSymbol == symbol)
+                    {
+                        hadPrevious = true;
+                        break;
+                    }
+                }
+                if (hadPrevious)
+                {
+                    continue;
+                }
+                else
+                {
+                    int count = 0;
+                    for (int x = 0; x < str.Length; x++)
+                    {
+                        char currentSymbol = str[x];
+                        if (currentSymbol == symbol)
+                        {
+                            count++;
+                        }
+                    }
+                    Console.WriteLine(symbol + " - " + count);
+                }
+            }
+        }
+    }
+
