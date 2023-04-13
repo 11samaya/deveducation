0826a62038e1c896e497df0cb55c88bb1f20de7b
11samaya <samayavtx@gmail.com>
2023-04-13 18:08:23 +0400

    added web application

---
+++ WebApplication4/WebApplication4/Controllers/WeatherForecastController.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+
+namespace Homework
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SorterController : ControllerBase
+    {
+        [HttpPost("Sort")]
+        public int[] Sort(int[] arr)
+        {
+            Sorter sorter = new Sorter();
+            return sorter.Sort(arr);
+        }
+    }
+    public class Sorter
+    {
+        public int[] Sort(int[] notSortedArray)
+        {
+            int[] sortedArray = notSortedArray;
+            int temp;
+            string notSortedString;
+
+            for (int j = 0; j <= sortedArray.Length - 2; j++)
+            {
+                for (int i = 0; i <= sortedArray.Length - 2; i++)
+                {
+                    if (sortedArray[i] > sortedArray[i + 1])
+                    {
+                        temp = sortedArray[i + 1];
+                        sortedArray[i + 1] = sortedArray[i];
+                        sortedArray[i] = temp;
+                    }
+                }
+            }
+
+            return sortedArray;
+        }
+        public double[] Sort(double[] notSortedArray)
+        {
+            double[] sortedArray = notSortedArray;
+            double temp;
+
+            for (int j = 0; j <= sortedArray.Length - 2; j++)
+            {
+                for (int i = 0; i <= sortedArray.Length - 2; i++)
+                {
+                    if (sortedArray[i] > sortedArray[i + 1])
+                    {
+                        temp = sortedArray[i + 1];
+                        sortedArray[i + 1] = sortedArray[i];
+                        sortedArray[i] = temp;
+                    }
+                }
+            }
+            return sortedArray;
+        }
+        public char[] Sort(char[] notSortedArray)
+        {
+            char[] sortedArray = notSortedArray;
+            char temp;
+            for (int j = 0; j <= sortedArray.Length - 2; j++)
+            {
+                for (int i = 0; i <= sortedArray.Length - 2; i++)
+                {
+                    if (sortedArray[i] > sortedArray[i + 1])
+                    {
+                        temp = sortedArray[i + 1];
+                        sortedArray[i + 1] = sortedArray[i];
+                        sortedArray[i] = temp;
+                    }
+                }
+            }
+            return sortedArray;
+        }
+        public string Sort(string notSortedArray)
+        {
+            var chars = notSortedArray.ToCharArray();
+            return string.Join("", Sort(chars));
+        }
+    }
+
+}
